9f941c0928a59a3eb2d89d7ea9dfc8ec6b41b9a0
Umayange1998 <wgikumayanga@gmail.com>
2025-10-15 03:06:50 +0530

    nodejs

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,8 @@
+import generateName  from "sillyname";
+
+const sillyName = generateName();
+console.log(`My name is ${sillyName}`);
+
+import { randomSuperhero } from "superheroes";
+const name = randomSuperhero();
+console.log(`I am ${name}`);
